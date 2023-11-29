2263df6b783a79716fcda7e339e92ea3c077a9fc
Samuel-Toriola <samueloladipupo.toriola@gmail.com>
2023-11-29 08:48:47 +0100

    New styling

--- script.js
+++ script.js
@@ -2,23 +2,30 @@
 const input = document.querySelector(".search");
 const submit = document.querySelector(".arrow");
 const mapDiv = document.querySelector("#map");
+const form = document.querySelector(".input__field");
 // const ipAddress = document.querySelector("#ip");
 const details = document.querySelector(".details");
 
-submit.addEventListener("click", function () {
+submit.addEventListener("click", function (e) {
+  e.preventDefault();
   input.value ? whereAmI(input.value) : whereAmI();
+});
+
+form.addEventListener("click", function (e) {
+  e.preventDefault();
 });
 
 const whereAmI = async function (ip = "") {
   try {
     const res = await fetch(
-      `https://geo.ipify.org/api/v2/country,city?apiKey=at_iV6a6Z9rMCmQ4SRPVzsUHv5hJeZdJ&ipAddress=${ip}`
+      `https://geo.ipify.org/api/v2/country,city?apiKey=at_dyNYUy1rsEkE1IRVN2xo5M2t5I3il&ipAddress=${ip}`
     );
     const data = await res.json();
+    console.log(data);
 
     if (!data.isp) {
       setTimeout(function () {
-        location.reload();
+        // location.reload();
       }, 3000);
       throw new Error(
         "Use a generalized Ip address, Like a state or country. Page will now reload"
@@ -28,16 +35,16 @@ const whereAmI = async function (ip = "") {
 
     const html = `
 
-        <ul>
-          <li class="details__list border">
+          
+          <li class="details__list ">
             <h4>IP ADDRESS</h4>
             <p id="ip">${data.ip}</p>
           </li>
-          <li class="details__list border">
+          <li class="details__list ">
             <h4>LOCATION</h4>
             <p id="location">${data.location.region}, ${data.location.country}</p>
           </li>
-          <li class="details__list border">
+          <li class="details__list add">
             <h4>TIMEZONE</h4>
             <p id="timezone">${data.location.timezone}</p>
           </li>
@@ -45,7 +52,8 @@ const whereAmI = async function (ip = "") {
             <h4>ISP</h4>
             <p id="isp">${data.isp}</p>
           </li>
-        </ul>
+          
+       
   `;
     details.textContent = "";
     details.insertAdjacentHTML("beforeend", html);
